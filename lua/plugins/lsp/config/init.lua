16865ddd032303f7b8c8aa51ce69ee95fe317ffd
AkagiYuu <khanghuynh0245@gmail.com>
2023-04-25 15:16:12 +0700

    Update

--- lua/plugins/lsp/config/init.lua
+++ lua/plugins/lsp/config/init.lua
@@ -95,22 +95,24 @@ local _lspconfig = {
 _lspconfig.config = function(_, opts)
     local servers = opts.servers
     local function setup(server)
-        local server_opts = vim.tbl_deep_extend('force', {
-            capabilities = vim.deepcopy(capabilities),
-        }, servers[server] or {})
+        local server_opts = servers[server]
+        servers.capabilities = capabilities
 
         require('lspconfig')[server].setup(server_opts)
     end
 
-    local mlsp = require('mason-lspconfig')
-    local available = mlsp.get_available_servers()
+    local have_mason, mlsp = pcall(require, 'mason-lspconfig')
+    local all_mslp_servers = {}
+    if have_mason then
+        all_mslp_servers = vim.tbl_keys(require('mason-lspconfig.mappings.server').lspconfig_to_package)
+    end
 
     local ensure_installed = {} ---@type string[]
     for server, server_opts in pairs(servers) do
         if server_opts then
             server_opts = server_opts == true and {} or server_opts
             -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
-            if server_opts.mason == false or not vim.tbl_contains(available, server) then
+            if server_opts.mason == false or not vim.tbl_contains(all_mslp_servers, server) then
                 setup(server)
             else
                 ensure_installed[#ensure_installed + 1] = server
@@ -118,8 +120,10 @@ _lspconfig.config = function(_, opts)
         end
     end
 
-    require('mason-lspconfig').setup { ensure_installed = ensure_installed }
-    require('mason-lspconfig').setup_handlers { setup }
+    if have_mason then
+        mlsp.setup { ensure_installed = ensure_installed }
+        mlsp.setup_handlers { setup }
+    end
 
     vim.api.nvim_create_autocmd('LspAttach', {
         desc = 'Global lsp on attach',
