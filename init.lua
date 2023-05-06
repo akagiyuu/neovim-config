7b5e7791aacc91f96b55c7e71e248b7e122233a4
AkagiYuu <khanghuynh0245@gmail.com>
2023-05-06 17:55:04 +0700

    Init

--- init.lua
+++ init.lua
@@ -8,20 +8,4 @@ vim.defer_fn(function()
 
     vim.cmd.rshada()
     vim.opt.spell = true
-    -- require('lspconfig').rust_analyzer.setup {
-    --
-    --     -- capabilities = capabilities,
-    --     -- on_attach = on_attach,
-    --     settings = {
-    --         ['rust-analyzer'] = {
-    --             checkOnSave = {
-    --                 command = 'clippy',
-    --             },
-    --             diagnostics = {
-    --                 enable = true,
-    --                 experimental = { enable = true }
-    --             }
-    --         }
-    --     }
-    -- }
 end, 750)
