8fd74b527f4d8a4e0d723c1217e7996004bc8ba5
akagiyuu <khanghuynh0245@gmail.com>
2024-12-25 23:19:21 +0700

    feat: remove syntax tree surfer

--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -14,60 +14,6 @@ return {
         config = true
     },
     {
-        'ziontee113/syntax-tree-surfer',
-        config = true,
-        keys = {
-            { 'vx',    '<cmd>STSSelectMasterNode<cr>',      mode = 'n', noremap = true },
-            { 'vn',    '<cmd>STSSelectCurrentNode<cr>',     mode = 'n', noremap = true },
-            { 'J',     '<cmd>STSSelectNextSiblingNode<cr>', mode = 'x', noremap = true },
-            { 'K',     '<cmd>STSSelectPrevSiblingNode<cr>', mode = 'x', noremap = true },
-            { 'H',     '<cmd>STSSelectParentNode<cr>',      mode = 'x', noremap = true },
-            { 'L',     '<cmd>STSSelectChildNode<cr>',       mode = 'x', noremap = true },
-            { '<A-j>', '<cmd>STSSwapNextVisual<cr>',        mode = 'x', noremap = true },
-            { '<A-k>', '<cmd>STSSwapPrevVisual<cr>',        mode = 'x', noremap = true },
-            {
-                'vU',
-                function()
-                    vim.opt.opfunc = 'v:lua.STSSwapUpNormal_Dot'
-                    return 'g@l'
-                end,
-                mode = 'n',
-                expr = true,
-                desc = 'Swap master node up',
-            },
-            {
-                'vD',
-                function()
-                    vim.opt.opfunc = 'v:lua.STSSwapDownNormal_Dot'
-                    return 'g@l'
-                end,
-                mode = 'n',
-                expr = true,
-                desc = 'Swap master node down',
-            },
-            {
-                'vu',
-                function()
-                    vim.opt.opfunc = 'v:lua.STSSwapCurrentNodePrevNormal_Dot'
-                    return 'g@l'
-                end,
-                mode = 'n',
-                expr = true,
-                desc = 'Swap current node down',
-            },
-            {
-                'vd',
-                function()
-                    vim.opt.opfunc = 'v:lua.STSSwapCurrentNodeNextNormal_Dot'
-                    return 'g@l'
-                end,
-                mode = 'n',
-                expr = true,
-                desc = 'Swap current node down',
-            },
-        }
-    },
-    {
         'folke/flash.nvim',
         opts = {},
         keys = {
