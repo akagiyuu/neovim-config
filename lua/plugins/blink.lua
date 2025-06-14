2691af2c03b1102274c296963b005d9220c332b6
akagiyuu <khanghuynh0245@gmail.com>
2025-06-14 11:15:40 +0700

    update

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -5,24 +5,7 @@ return {
         'mikavilpas/blink-ripgrep.nvim',
     },
     event = { 'InsertEnter', 'CmdlineEnter' },
-    build = 'cargo build --release',
     opts = {
-        keymap = {
-            preset = 'none',
-            ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-            ['<C-e>'] = { 'hide', 'fallback' },
-            ['<CR>'] = { 'accept', 'fallback' },
-
-            ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-            ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-
-            ['<C-p>'] = { 'select_prev', 'fallback' },
-            ['<C-n>'] = { 'select_next', 'fallback' },
-            ['<Up>'] = { 'select_prev', 'fallback' },
-            ['<Down>'] = { 'select_next', 'fallback' },
-            ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
-            ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
-        },
         appearance = {
             use_nvim_cmp_as_default = false,
             nerd_font_variant = 'mono'
@@ -46,6 +29,15 @@ return {
                         prefix_min_len = 5,
                     }
                 }
+            },
+        },
+        fuzzy = {
+            implementation = 'rust',
+            sorts = {
+                'exact',
+                -- defaults
+                'score',
+                'sort_text',
             },
         },
         completion = {
